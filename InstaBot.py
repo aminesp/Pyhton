9a16f82f6b64c57abdcaa822d3fd745ddc865604
aminesp <84672204+aminesp@users.noreply.github.com>
2022-10-13 16:49:53 +0100

    Create InstaBot.py

---
+++ InstaBot.py
@@ -0,0 +1,7 @@
+# pip install instabot 
+From instabot import Bot 
+bot = Bot()
+bot.login(username = "username")
+password = "password")
+bot.upload_photo(your.photo.jpg
+caption = "your caption of post here" )
